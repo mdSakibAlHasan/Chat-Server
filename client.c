73b42489cf51929fb55cd2718773a6b5ed28e55f
Sakib Al Hasan <76566629+mdSakibAlHasan@users.noreply.github.com>
2022-09-14 00:43:49 +0600

    chat with everyone
    But exit not work.
    Next found list,unique name,change variable and function name

--- client.c
+++ client.c
@@ -1,15 +1,3 @@
-/*
- * client.c -- a stream socket client demo
- */
-
-/*
- * The steps involved in establishing a socket on the client side are as follows:
- *  1. Create a socket with the socket() system call
- *  2. Connect the socket to the address of the server using the connect() system call
- *  3. Send and receive data. There are a number of ways to do this, but the simplest 
- *     is to use the read() and write() system calls.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -28,18 +16,18 @@ char client_name[20];
 void *dostuff (void *socket_desc)
 {
   char *str, buffer[MAXDATASIZE];
-  //Get the socket descriptor
   int sock = *(int*)socket_desc,numbytes;
-while(1){
-   bzero(buffer,MAXDATASIZE);
-    if ((numbytes=read(sock, buffer, MAXDATASIZE-1)) < 0) {
-        perror("ERROR reading from socket");  exit(0);  }
+    
+   while(1){
+        bzero(buffer,MAXDATASIZE);
+        if ((numbytes=read(sock, buffer, MAXDATASIZE-1)) < 0) {
+            perror("ERROR reading from socket");  
+            exit(0);  
+        }
         buffer[numbytes] = '\0';
 
-    if(strlen(buffer) !=0)    
-        printf("Recieve %s\n",buffer);
-}
-
+        printf("From: %s\n",buffer);
+    }
 
 }
 
@@ -53,15 +41,20 @@ int main(int argc, char *argv[])
     pthread_t thread_id;
 
     if (argc != 3) {
-        printf("usage: client hostname\n"); exit(1);  
+        printf("usage: client hostname and name\n"); 
+        exit(1);  
     }
     
 
     if ((server=gethostbyname(argv[1])) == NULL) {  // get the host info 
-        printf("ERROR, no such host\n");  exit(0);  }
+        printf("ERROR, no such host\n");  
+        exit(0);  
+    }
 
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("ERROR opening socket");  exit(0);  }
+        perror("ERROR opening socket");  
+        exit(0);  
+    }
 
     bzero((char *) &server_addr, sizeof(server_addr));  // zero the rest of the struct 
     server_addr.sin_family = AF_INET;    // host byte order 
@@ -69,35 +62,39 @@ int main(int argc, char *argv[])
     server_addr.sin_addr = *((struct in_addr *)server->h_addr);
 
     if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) < 0) {
-        perror("ERROR connecting");  exit(0);  }
+        perror("ERROR connecting");  
+        exit(0);  
+    }
 
     if (write(sockfd,argv[2],strlen(argv[2])) < 0) {        //sent client name to server
-        perror("ERROR writing to socket"); exit(0);  
+        perror("ERROR writing to socket"); 
+        exit(0);  
     }
 
 
    if( pthread_create( &thread_id , NULL ,  dostuff , (void*) &sockfd) < 0) {           //create thread for read
-        perror("ERROR create thread"); exit(1);  }
-
-    while(1){
-    bzero(buf,MAXDATASIZE);
-    fgets(buf,MAXDATASIZE-1,stdin);
-
-    // strcpy(temp,argv[2]);
-    // int length = strlen(temp);
-    // temp[length] = 32;          //add a space after client name
-    // temp[length+1] = '\0';
-    // strcat(temp,buf);
-
-    if (write(sockfd,buf,strlen(buf)) < 0) {
-        perror("ERROR writing to socket"); exit(0);  }
-
+        perror("ERROR create thread"); 
+        exit(1);  
     }
 
-   
+    while(1){
+        bzero(buf,MAXDATASIZE);
+        fgets(buf,MAXDATASIZE-1,stdin);
+
+        if(strcmp(buf,"exit\n")==0){
+            printf("Close connection\n");
+            pthread_exit(&thread_id);
+            close(sockfd);
+            exit(0);
+        }
+    
+        if (write(sockfd,buf,strlen(buf)) < 0) {
+            perror("ERROR writing to socket"); 
+            exit(0);  
+        }
+    }
 
     close(sockfd);
-
     return 0;
 }
 
--- server.c
+++ server.c
@@ -1,23 +1,3 @@
-/*
- * server.c -- a stream socket server demo
- */
-
-/*
- * The steps involved in establishing a socket on the server side are as follows:
- *  1. Create a socket with the socket() system call
- *  2. Bind the socket to an address using the bind() system call. For a server socket
- *     on the Internet, an address consists of a port number on the host machine.
- *  3. Listen for connections with the listen() system call
- *  4. Accept a connection with the accept() system call. This call typically blocks
- *     until a client connects with the server.
- *  5. Send and receive data
- *
- *  Ref:
- *   1. https://www.linuxhowtos.org/C_C++/socket.htm
- *   2. http://www.mario-konrad.ch/blog/programming/multithread/tutorial-04.html
- */
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -36,10 +16,9 @@
 void *dostuff(void *); //the thread function
 
 struct client_struct{
-    char client_name[8];
+    char client_name[20];
     int socket_number;
 };
-
 struct client_struct client_info[MAXCLIENT];
 int number_of_client=0;
 
@@ -52,7 +31,9 @@ int main(void)
   pthread_t thread_id;
 
   if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-      perror("ERROR opening socket"); exit(1);  }
+      perror("ERROR opening socket"); 
+      exit(1);  
+  }
 
   bzero((char *) &my_addr, sizeof(my_addr)); // zero the rest of the struct
 
@@ -61,21 +42,29 @@ int main(void)
   my_addr.sin_port = htons(MYPORT);     // short, network byte order
 
   if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) < 0) {
-      perror("ERROR on binding"); exit(1);  }
+      perror("ERROR on binding"); 
+      exit(1);
+  }
 
   if (listen(sockfd, BACKLOG) < 0) {
-      perror("ERROR on listen"); exit(1); }
+      perror("ERROR on listen"); 
+      exit(1); 
+  }
 
   sin_size = sizeof(struct sockaddr_in);
 
   while (1) {
     if ((new_sockfd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size)) < 0) {
-        perror("ERROR on accept"); exit(1);  }
-    printf("server: got connection from %s and port %d\n",inet_ntoa(their_addr.sin_addr),their_addr.sin_port);
+        perror("ERROR on accept"); 
+        exit(1);  
+    }
+    printf("server: got connection from %s, port %d and ",inet_ntoa(their_addr.sin_addr),their_addr.sin_port);
 
     // start child thread
     if( pthread_create( &thread_id , NULL ,  dostuff , (void*) &new_sockfd) < 0) {
-        perror("ERROR create thread"); exit(1);  }
+        perror("ERROR create thread");
+        exit(1);  
+    }
 
     pthread_detach(thread_id);  // don't track it
 
@@ -86,26 +75,33 @@ int main(void)
 }
 
 
-int extract_name_message(char  str[])
+int extract_name_message(char  str[], int client_index)
 {
-  printf("%s\n",str);
-  if(str[0] != '@'){
-    printf("Error client name message\n");
+  if(str[0] != '@'){                    //message must be start with @
+    printf("Error message syntax\n");
     return -1;
   }
 
-  char client_name[8],index=0,i=1;
-  while(str[i] != 32){
+  char client_name[MAXCLIENT],sending_mesage[MAXDATASIZE],index=0,i=1;
+
+  while(str[i] != 32 && str[i] != '\n'){                      //separate name and message
       client_name[index++] = str[i++];
   }
   client_name[index] = '\0';
 
-  for(index=0;str[i];i++){
+  for(index=0;str[i];i++){              //aline message
     str[index++] = str[i];
   }
   str[index] = '\0';
 
-  for(int k=0;k<number_of_client;k++){
+  strcpy(sending_mesage,client_info[client_index].client_name);     //aline message format
+  strcat(sending_mesage,"-> ");
+  strcat(sending_mesage,str);
+  strcpy(str,sending_mesage);
+
+
+
+  for(int k=0;k<number_of_client;k++){                              //serach sender index and socket number
     if(strcmp(client_info[k].client_name,client_name) == 0){
       return k;//client_info[k].socket_number;
     }
@@ -115,18 +111,18 @@ int extract_name_message(char  str[])
       return -2;
 
   return -1;
-  //send_specific(client_name,str);
-  //printf("%s\t%s\n",client_name,str);
 }
 
 
-void send_all(char str[])
+void send_all(char str[],int client_index)
 {
     for(int i=0;i<number_of_client;i++){
-        if (write(client_info[i].socket_number, str, strlen(str)) < 0) {
-              printf("ERROR writing to %s\n",client_info[i].client_name);
-        }
+      if(client_index == i)                   //self message not send
+        continue;
 
+      if (write(client_info[i].socket_number, str, strlen(str)) < 0) {        //send mesage all client listed
+          printf("ERROR writing to %s\n",client_info[i].client_name);
+      }
     }
 }
 
@@ -134,20 +130,24 @@ void send_all(char str[])
 
 void sent_message_client(int client_index,char str[])
 {
-    int ci = extract_name_message(str);
-    if(ci == -1){
-        char warning_message[]={"Client not found\n"};
+    int sock_index = extract_name_message(str,client_index);
+    if(sock_index == -1){
+        char warning_message[]={"Server-> Client not found\n"};
         write(client_info[client_index].socket_number, warning_message, strlen(warning_message));
     }
-    else if(ci == -2)
-        send_all(str);
-    //char str[MAXDATASIZE];
-
-    if (write(client_info[ci].socket_number, str, strlen(str)) < 0) {
-      perror("ERROR writing to socket"); exit(1);  }
+    if(sock_index == -3){
+        char warning_message[]={"Server-> Syntex error\n"};
+        write(client_info[client_index].socket_number, warning_message, strlen(warning_message));
+    }
+    else if(sock_index == -2)
+        send_all(str,client_index);
+    
+    if (write(client_info[sock_index].socket_number, str, strlen(str)) < 0) {
+      perror("ERROR writing to socket"); 
+      exit(1);  
+    }
 
       return ;
-
 }
 
 
@@ -166,39 +166,39 @@ void *dostuff (void *socket_desc)
 {
   int n = number_of_client;
   char *str, buffer[MAXDATASIZE];
-  //Get the socket descriptor
   int sock = *(int*)socket_desc;
+
+
   printf("socket number %d\n",sock);
-
   bzero(buffer,MAXDATASIZE);
+
   if (read(sock,buffer,MAXDATASIZE-1) < 0) {                //recieve name from client and save it struct
-      perror("ERROR reading from socket"); exit(1);  }
-  printf("Here is the connection : %s\n",buffer);
-  strcpy(client_info[number_of_client].client_name,buffer);
-    client_info[number_of_client++].socket_number = sock;
+      perror("ERROR reading from socket"); 
+      exit(1);  
+  }
+  
+  strcpy(client_info[number_of_client].client_name,buffer);       //list client information
+  client_info[number_of_client++].socket_number = sock;
 
-    while(1){
-  str = "Please enter the message: ";
+  str = "Server-> Connection established\n";
   if (write(sock, str, strlen(str)) < 0) {
-      perror("ERROR writing to socket"); exit(1);  }
+      perror("ERROR writing to socket"); 
+      exit(1);  
+  }
 
 
-
-  bzero(buffer,MAXDATASIZE);
-  if (read(sock,buffer,MAXDATASIZE-1) < 0) {
-      perror("ERROR reading from socket"); exit(1);  }
-  printf("Here is the message: %s\n",buffer);
+  while(1){
+    bzero(buffer,MAXDATASIZE);
+    if (read(sock,buffer,MAXDATASIZE-1) < 0) {
+      perror("ERROR reading from socket"); 
+      exit(1);  
+    }
+    printf("Here is the message: %s\n",buffer);
 
     sent_message_client(n,buffer);      //sent mesage other client
-    }
-
-//   str = "I got your message \n";
-//   if (write(sock, str, strlen(str)) < 0) {
-     // perror("ERROR writing to socket"); exit(1);  }
+  }
 
   close(sock);
   pthread_exit(NULL);
 }
-
-
 
